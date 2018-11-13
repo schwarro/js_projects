4de2d7fb1afd3a80ae0cdb94557b6fe43dfb3e48
schwarro <rob.ap.schwartz@gmail.com>
2018-11-13 15:44:22 -0500

    played around with js objects

---
+++ project8/script.js
@@ -0,0 +1,48 @@
+//
+// let name1 = "Rob";
+// let eyeColor1 = "Blue";
+// let age1 = 30;
+//
+// let name2 = "David";
+// let eyeColor2 = "Green";
+// let age2 = 28;
+//
+// let name3 = "Matthew";
+// let eyeColor3 = "Brown";
+// let age3 = 25;
+//
+// let updateAge = function(age) {
+//   return ++age;
+// }
+
+
+
+
+// let person = new Object();
+//
+// person.name = "Rob";
+// person.eyeColor = "Blue";
+// person.age = 30;
+// person.updateAge = function() {
+//   return ++person.age
+// }
+//
+// console.log(person.age);
+// person.updateAge();
+// console.log(person.age);
+
+
+
+
+let person = {
+  name: "Daniel",
+  eyeColor: "Blue",
+  age: 30,
+  updateAge: function() {
+    return ++person.age;
+  }
+}
+
+console.log(person);
+person.updateAge();
+console.log(person.age);99999999999980
