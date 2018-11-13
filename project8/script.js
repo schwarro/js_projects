1d5c3ed9c8b558b9442d554ae0029ff00a1cc651
schwarro <rob.ap.schwartz@gmail.com>
2018-11-13 16:51:02 -0500

    created dice roller with js

--- project8/script.js
+++ project8/script.js
@@ -1,48 +1,19 @@
-//
-// let name1 = "Rob";
-// let eyeColor1 = "Blue";
-// let age1 = 30;
-//
-// let name2 = "David";
-// let eyeColor2 = "Green";
-// let age2 = 28;
-//
-// let name3 = "Matthew";
-// let eyeColor3 = "Brown";
-// let age3 = 25;
-//
-// let updateAge = function(age) {
-//   return ++age;
-// }
-
-
-
-
-// let person = new Object();
-//
-// person.name = "Rob";
-// person.eyeColor = "Blue";
-// person.age = 30;
-// person.updateAge = function() {
-//   return ++person.age
-// }
-//
-// console.log(person.age);
-// person.updateAge();
-// console.log(person.age);
-
-
-
-
-let person = {
-  name: "Daniel",
-  eyeColor: "Blue",
-  age: 30,
-  updateAge: function() {
-    return ++person.age;
+var dice = {
+  sides: 6,
+  roll: function() {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
   }
 }
 
-console.log(person);
-person.updateAge();
-console.log(person.age);99999999999980
+function printNumber(number) {
+  var placeholder = document.getElementById("placeholder");
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById("button");
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
