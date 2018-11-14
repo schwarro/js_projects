b428d30b130d92f13dbcf9a9f5d98abdae2fd8b4
schwarro <rob.ap.schwartz@gmail.com>
2018-11-14 13:03:17 -0500

    made rock paper scissors with js

---
+++ project10/script.js
@@ -0,0 +1,44 @@
+function pick() {
+  var computer = ["Rock", "Paper", "Scissors"];
+  var userSelection = document.querySelector('#operator').value;
+  var computerSelection = computer[Math.floor(Math.random()*computer.length)];
+  var button = document.getElementById("button");
+  var result;
+
+
+  if (userSelection == "Rock" && computerSelection == "Rock") {
+    result = "Try Again";
+  } else if (userSelection == "Rock" && computerSelection == "Paper") {
+    result = "You Lose" ;
+  } else if (userSelection == "Rock" && computerSelection == "Scissors") {
+    result = "You Win";
+  } else if (userSelection == "Paper" && computerSelection == "Rock") {
+    result = "You Win";
+  } else if (userSelection == "Paper" && computerSelection == "Paper") {
+    result = "Try Again";
+  } else if (userSelection == "Paper" && computerSelection == "Scissors") {
+    result = "You Lose";
+  } else if (userSelection == "Scissors" && computerSelection == "Rock") {
+    result = "You Lose";
+  } else if (userSelection == "Scissors" && computerSelection == "Scissors") {
+    result = "Try Again";
+  } else if (userSelection == "Scissors" && computerSelection == "Paper") {
+    result = "You Win";
+  }
+  document.querySelector("#results").innerHTML = result;
+  document.querySelector("#placeholder").innerHTML = computerSelection;
+
+}
+
+
+// function printString(string) {
+//   var placeholder = document.getElementById("placeholder");
+//   placeholder.innerHTML = number;
+// }
+//
+// var button = document.getElementById("button");
+//
+// button.onclick = function() {
+//   var result = dice.roll();
+//   printNumber(result);
+// };
