fc6dc62d7f9c21e6aa14cf62257e6d23d0808d0c
schwarro <rob.ap.schwartz@gmail.com>
2018-12-05 14:14:07 -0500

    created js password checker

---
+++ project22/script.js
@@ -0,0 +1,19 @@
+var password = "wordpass";
+var response;
+var entryCount = 0;
+var entryLimit = 3;
+var error = false;
+
+while(response != password && !error){
+  if(entryCount < entryLimit){
+    response = window.prompt("Enter Password");
+    entryCount++;
+  } else {
+    error = true;
+  }
+}
+if(error){
+  alert("Out of Guesses")
+} else {
+alert("Password Accepted!");
+}
