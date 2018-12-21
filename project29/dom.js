be7003439b9127c49631bc84bc4a760dc4fe7069
schwarro <rob.ap.schwartz@gmail.com>
2018-12-21 14:00:30 -0500

    created list that can add, remove and filter items with js

--- project29/dom.js
+++ project29/dom.js
@@ -202,4 +202,73 @@
   // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
 
   // box.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',50)';
+// }
+
+/////////////////////////////////////////////////////////////
+
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// form submit event
+form.addEventListener('submit', addItem);
+//delete event
+itemList.addEventListener('click', removeItem);
+//filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  //get input value
+  var newItem =document.getElementById('item').value;
+
+  //create new li Element
+  var li = document.createElement('li');
+  //add class
+  li.className = 'list-group-item';
+  //add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  //create del button Element
+  var deleteBtn = document.createElement('button');
+
+  //add classes to delete button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  //append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  //append button to li
+  li.appendChild(deleteBtn);
+
+  //append li to list
+  itemList.appendChild(li);
+}
+
+//remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+//filter items
+function filterItems(e){
+  //convert to lower case
+  var text = e.target.value.toLowerCase();
+  //get list
+  var items = itemList.getElementsByTagName('li');
+  //convert array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
